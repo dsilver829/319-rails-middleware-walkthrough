52396a7e0648e48fa531dc47a534ea83c162c52d
David Silver <dsilver@candidatemetrics.com>
2012-08-14 10:18:59 -0700

    added generic rails app

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '5ea8918e1b02f2d09e7a9d5ea8f19598c6065be1c63874f274e12aa6ce82681b5040ad3a2fff2c76cf4d839a5efbf8fd422d2b9477af2e374b53b9adf0064972'
